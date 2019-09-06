92bb7c95b4514fe206fe0426b734e5ebad0e590b
leyla li <leylali@qq.com>
2019-09-06 08:53:44 -0700

    almost done with coding

--- 401-challenges/binaryTreePathLength/findPathLength.js
+++ 401-challenges/binaryTreePathLength/findPathLength.js
@@ -1,11 +1,34 @@
 'use strict';
 
-function findPathLength (root, parent, child){
-    if(!root){
-        return null;
-    }else{
-        
+class Tree {
+    constructor(root){
+        this.root = root;
+        this.left = null;
+        this.right = null;
     }
 }
 
-module.exports = findPathLength;
+function findPathLength (root, parent, child, isParent=false, count = 0){
+    if(!root){
+        return null;
+    }else{
+        if(root.value === parent){
+            isParent = true;
+        }
+        if(isParent){
+            if(root.left){
+              findPathLength(root.left, parent, child, count+1);
+            }
+            if(root.right){
+              findPathLength(root.right, parent, child, count+1);
+            }
+        }
+        if(root.value === child){
+            return count;
+        }
+        findPathLength(root.left, parent, child, count);
+        findPathLength(root.right, parent, child, count);
+    }
+}
+
+module.exports = {Tree: Tree, findPathLength: findPathLength};
