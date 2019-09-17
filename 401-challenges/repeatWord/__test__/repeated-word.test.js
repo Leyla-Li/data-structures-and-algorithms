779ea7eba11434ec6e2c3f08b5d25dbe6a741055
leyla li <leylali@qq.com>
2019-09-16 17:09:42 -0700

    done with coding and testing

---
+++ 401-challenges/repeatWord/__test__/repeated-word.test.js
@@ -0,0 +1,25 @@
+'use strict';
+ 
+const functions = require('../repeated-word');
+const getWords = functions.getWords;
+const firstRepeated = functions.firstRepeated;
+
+describe('test for finding the first word that is repeated within the given string', ()=>{
+
+  it('returns empty string if there is no repeated word', ()=>{
+    let string = 'hello there';
+    expect(getWords(string)).toEqual('');
+  });
+
+  it('returns the first repeated word', ()=>{
+    let string = 'hello there hello';
+    expect(getWords(string)).toEqual('hello');
+  });
+
+  it('only returns the first repeated word', ()=>{
+    let string = 'hello there hello there';
+    expect(getWords(string)).toEqual('hello');
+  });
+  
+
+});
---
+++ 401-challenges/repeatWord/repeated-word.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let arr1 = [];
+let word = '';
+
+function getWords (str){
+    const length = str.length;
+    
+    for(let i = 0; i <= length; i++){
+        if(str[i] !== ' ' && i !== length){
+            word += str[i];
+        }else{
+            arr1.push(word);
+            word = '';
+        }
+    }
+    return firstRepeated(arr1);
+}
+
+function firstRepeated (arr1){
+    const length = arr1.length;
+    for(let i = 0; i <= length; i++){
+        for(let j = i + 1; j < length; j++){
+            if(arr1[j] === arr1[i]){
+                return arr1[j];
+            }
+        }
+    }
+    return '';
+}
+
+module.exports = {getWords: getWords, firstRepeated:firstRepeated};
