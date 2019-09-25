3260779e67249349d40c7f2543b013b36233b670
leyla li <leylali@qq.com>
2019-09-24 21:58:57 -0700

    fixing some errors

--- 401-challenges/treeIntersection/tree-intersection.js
+++ 401-challenges/treeIntersection/tree-intersection.js
@@ -11,30 +11,37 @@ class Tree {
     values.add(root.value);
     if(root.left){
       values.add(root.left.value);
-      this.getVal(root.left, values)
+      this.getVal(root.left, values);
     }
     if(root.right){
-        values.add(root.right.value);
-        this.getVal(root.right, values)
-      }
+      values.add(root.right.value);
+      this.getVal(root.right, values);
+    }
     return values;
   }
 
   validation (root, values, intersection) {
     if(values.has(root.value)){
       intersection.add(root.value);
+    }else{
+      values.add(root.value);
     }
+
     if(root.left){
       if(values.has(root.left.value)){
-        intersection.add(root.left.value);
+        this.validation (root.left, values, intersection.add(root.left.value));
+      }else{
+        this.validation (root.left, values.add(root.left.value), intersection);
       }
-      values.add(root.left.value);
+      
     }
+
     if(root.right){
       if(values.has(root.right.value)){
-        intersection.add(root.right.value);
+        this.validation (root.right, values, intersection.add(root.right.value));
+      }else{
+        this.validation (root.right, values.add(root.right.value), intersection);
       }
-      values.add(root.right.value);
     }
     console.log('this is the all the values and inter', values, intersection);
     return {'values': values, 'intersection': intersection};
@@ -52,8 +59,9 @@ class Tree {
       return {'intersection: ': null, 'union: ': this.getVal(root2, values)};
     }else if(root1 && root2) {
       valOfTree = this.getVal(root1, values);
-      console.log(this.validation(root2,valOfTree, intersection))
-      this.validation(root2,valOfTree, intersection);
+    //   console.log(valOfTree);
+    //   console.log(this.validation(root2,valOfTree, intersection));
+      this.validation(root2, valOfTree, intersection);
     }
     console.log(valOfTree, intersection);
     return {valOfTree, intersection};
